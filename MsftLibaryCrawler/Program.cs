976dd6cd1afcd31cc10e0fbf8396e0bcf849cb9e
WalterMitty <jhy100.nk@gmail.com>
2016-04-30 17:05:59 +0800

    write line TItle & Url.

--- MsftLibaryCrawler/Program.cs
+++ MsftLibaryCrawler/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using HtmlAgilityPack;
+using static Newtonsoft.Json.JsonConvert;
+using static System.Console;
 
 namespace MsftLibaryCrawler
 {
@@ -10,6 +14,31 @@ namespace MsftLibaryCrawler
     {
         static void Main(string[] args)
         {
+            var startUrl = "https://msdn.microsoft.com/en-us/library/aa338032(v=vs.60).aspx";
+            StartAsync(startUrl);
+
+            Read();
         }
+
+        static async Task StartAsync(string url)
+        {
+            var tocUrl = url + "?toc=1";
+            var client = new HttpClient();
+            var json = await client.GetStringAsync(tocUrl);
+            
+            var subItems = DeserializeObject<SubItem[]>(json);
+            if (subItems == null)
+                return;
+
+            foreach (var subItem in subItems)
+            {
+                WriteLine($"{subItem.Title}: {subItem.Href}");
+                await StartAsync(subItem.Href);
+            }
+        }
+
+        //var hd = new HtmlDocument();
+        //hd.LoadHtml(html);
+        //var aNodes = hd.DocumentNode.SelectNodes("//a[@href][@title]");
     }
 }
---
+++ MsftLibaryCrawler/SubItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsftLibaryCrawler
+{
+
+    public class SubItem
+    {
+        public string Title { get; set; }
+        public string Href { get; set; }
+        public string ToolTip { get; set; }
+        public Extendedattributes ExtendedAttributes { get; set; }
+    }
+
+    public class Extendedattributes
+    {
+        public string datatochassubtree { get; set; }
+        public string datamtpsaliasid { get; set; }
+        public string datamtpsassetid { get; set; }
+        public string datamtpsshortid { get; set; }
+    }
+
+}
